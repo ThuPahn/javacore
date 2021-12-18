502a713f8989e50eff8a867a65a6c99c2e5d08c8
Thu <phansangthu@gmail.com>
2021-12-18 17:22:13 +0700

    a

---
+++ out/production/Java/b1.java
@@ -0,0 +1,16 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class b1 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 2;
+        int tong = x + y;
+        int hieu = x - y;
+        int tich = x * y;
+        int thuong = x / y;
+        System.out.println(tong);
+        System.out.println(hieu);
+        System.out.println(tich);
+        System.out.println(thuong);
+    }
+}
